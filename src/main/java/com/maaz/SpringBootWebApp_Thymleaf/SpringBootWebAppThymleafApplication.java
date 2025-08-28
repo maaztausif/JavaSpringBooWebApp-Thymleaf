12ff68a9f36067fc7171b59ab86b61eec29c9303
maaztausif <maaztausif48@gmail.com>
2025-08-28 19:01:10 +0200

    Project created

---
+++ src/main/java/com/maaz/SpringBootWebApp_Thymleaf/SpringBootWebAppThymleafApplication.java
@@ -0,0 +1,13 @@
+package com.maaz.SpringBootWebApp_Thymleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebAppThymleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebAppThymleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maaz/SpringBootWebApp_Thymleaf/SpringBootWebAppThymleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maaz.SpringBootWebApp_Thymleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebAppThymleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
